3c0428e929a72f5f9efecf6c90a92d2129d2be94
Kojiro So <kojiro.soooo@gmail.com>
2023-06-09 18:11:15 -0400

    added custom suggestions based on thresholds for each field, slightly modified UI for suggestions

--- src/App.js
+++ src/App.js
@@ -5,10 +5,30 @@ import CarbonFootprintImg from "./images/carbon-footprint.png";
 
 const App = () => {
   const [carbonFootprint, setCarbonFootprint] = useState(null);
+  const [electricity, setElectricity] = useState('')
+  const [naturalGas, setNaturalGas] = useState('')
+  const [fuel, setFuel] = useState('')
+  const [meals, setMeals] = useState('')
+  const [flights, setFlights] = useState('')
+  const [carMileage, setCarMileage] = useState('')
   const [showPopup, setShowPopup] = useState(false);
 
-  const handleCarbonFootprint = (carbonFootprint) => {
+  const handleCarbonFootprint = (
+    carbonFootprint,
+    electricity,
+    naturalGas,
+    fuel,
+    meals,
+    flights,
+    carMileage
+  ) => {
     setCarbonFootprint(carbonFootprint);
+    setElectricity(electricity);
+    setNaturalGas(naturalGas);
+    setFuel(fuel);
+    setMeals(meals);
+    setFlights(flights);
+    setCarMileage(carMileage);
     setShowPopup(true);
   };
 
@@ -42,8 +62,8 @@ const App = () => {
               </p>
               <br></br>
               <p>
-                Enter your information below to see your average monthly carbon footprint
-                (reprsented as CO<sub>2</sub>e ).
+                Enter your information below to see your average monthly carbon
+                footprint (reprsented as CO<sub>2</sub>e ).
               </p>
             </div>
           </div>
@@ -51,23 +71,35 @@ const App = () => {
             <CarbonFootprintForm onCalculate={handleCarbonFootprint} />
             {showPopup && (
               <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
-                <div className="bg-white rounded-lg p-6 max-w-xl">
-                  <h2 className="text-lg font-bold my-5">
-                    Your average monthly carbon footprint: {carbonFootprint} kgs CO<sub>2</sub>e
+                <div className="bg-white rounded-lg p-10 max-w-xl">
+                  <h2 className="text-lg text-center font-bold my-5">
+                    Your average monthly carbon footprint: <br /> {carbonFootprint} kgs
+                    CO<sub>2</sub>e
                   </h2>
                   <h3 className="mb-4">
-                    Relative to the average American, 
-                    {carbonFootprint <= 1400 && " your carbon footprint is low. "}
+                    Relative to the average American,
+                    {carbonFootprint <= 1400 &&
+                      " your carbon footprint is low. "}
                     {carbonFootprint > 1400 &&
                       carbonFootprint <= 1600 &&
                       " your carbon footprint is moderate. "}
-                    {carbonFootprint > 1600 && " your carbon footprint is high. "}
-                    Based on the questions we ask, an average American's carbon footprint is about 1489kgs CO<sub>2</sub>e.
+                    {carbonFootprint > 1600 &&
+                      " your carbon footprint is high. "}
+                    Based on the questions we ask, an average American's carbon
+                    footprint is about 1489kgs CO<sub>2</sub>e.
                   </h3>
-                  <h3 className="text-lg font-bold mb-2">
+                  <h3 className="text-lg text-center font-bold mb-2">
                     Suggestions for reducing your carbon footprint:
                   </h3>
-                  <Suggestions carbonFootprint={carbonFootprint} />
+                  <Suggestions
+                    carbonFootprint={carbonFootprint}
+                    electricity={electricity}
+                    naturalGas={naturalGas}
+                    fuel={fuel}
+                    meals={meals}
+                    flights={flights}
+                    carMileage={carMileage}
+                  />
                   <div className="flex justify-center">
                     <button
                       className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mt-4"
